4b0b337aacaeed7d12a3a0c19528fd66ce9ba9d3
KhunRen <rendidi.abc@gmail.com>
2025-05-05 21:58:35 +0700

    [Fix] Fix Some Test Automation

--- src/test/java/com/example/kalkulatorkel3/KalkulatorTestBagi.java
+++ src/test/java/com/example/kalkulatorkel3/KalkulatorTestBagi.java
@@ -8,9 +8,48 @@ public class KalkulatorTestBagi {
     private final Operasi operator = new Operasi();
 
     @Test
-    void testBagi() {
-        assertEquals(2.0, operator.bagi(10, 5));
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> operator.bagi(10, 0));
-        assertEquals("Pembagi tidak boleh nol", exception.getMessage());
+    void testBagiDuaBilanganPositif() {
+        assertEquals(1.0, operator.bagi(1.0, 1.0));
     }
+
+    @Test
+    void testBagiPositifNegatif() {
+        assertEquals(-1.0, operator.bagi(3.0, -3.0));
+    }
+
+    @Test
+    void testBagiNegatifNegatif() {
+        assertEquals(1.0, operator.bagi(-6.0, -6.0));
+    }
+
+    @Test
+    void testBagiPositifDenganKarakter() {
+        Exception e = assertThrows(IllegalArgumentException.class, () -> operator.bagi(2.0, "A"));
+        assertEquals("Input harus berupa angka", e.getMessage());
+    }
+
+    @Test
+    void testBagiNegatifDenganKarakter() {
+        Exception e = assertThrows(IllegalArgumentException.class, () -> operator.bagi(-2.0, "A"));
+        assertEquals("Input harus berupa angka", e.getMessage());
+    }
+
+    @Test
+    void testBagiKarakterDenganPositif() {
+        Exception e = assertThrows(IllegalArgumentException.class, () -> operator.bagi("A", 3.0));
+        assertEquals("Input harus berupa angka", e.getMessage());
+    }
+
+    @Test
+    void testBagiKarakterDenganNegatif() {
+        Exception e = assertThrows(IllegalArgumentException.class, () -> operator.bagi("A", -3.0));
+        assertEquals("Input harus berupa angka", e.getMessage());
+    }
+
+    @Test
+    void testBagiKarakterDenganNol() {
+        Exception e = assertThrows(IllegalArgumentException.class, () -> operator.bagi("A", 0.0));
+        assertEquals("Input harus berupa angka", e.getMessage());
+    }
+
 }
